8351dfeecc7df13b906784406b004ec5b5251eac
johnson7788 <linghuchongxajh@gmail.com>
2020-08-25 17:02:45 +0800

    add files

--- save_model.py
+++ save_model.py
@@ -3,7 +3,6 @@ import argparse
 from transformers import AutoTokenizer, AutoModel, AutoConfig
 import os
 
-
 def load_save_model(model_name, des_path):
     if not os.path.exists(des_path):
         os.makedirs(des_path)
@@ -18,6 +17,9 @@ def load_save_model(model_name, des_path):
     tokenizer.save_pretrained(des_path)
     config.save_pretrained(des_path)
     model.save_pretrained(des_path)
+    #删除不需要的配置，防止不兼容
+    os.remove(os.path.join(des_path,'tokenizer_config.json'))
+    os.remove(os.path.join(des_path,'special_tokens_map.json'))
     print(f'模型 {model_name} 已保存到 {des_path} ')
 
 if __name__ == '__main__':
